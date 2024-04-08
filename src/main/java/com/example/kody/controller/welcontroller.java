5df610d3dbae5b26adb7489e8a4373b0050d0ecb
opinionist <kminsung0728@gmail.com>
2024-04-09 07:43:42 +0900

    크로스 오리진을 메소드에 하나씩 정의하기

--- src/main/java/com/example/kody/controller/welcontroller.java
+++ src/main/java/com/example/kody/controller/welcontroller.java
@@ -1,11 +1,9 @@
 package com.example.kody.controller;
 
-
 import com.example.kody.service.Service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 
@@ -13,34 +11,36 @@ import java.util.Map;
 @RequestMapping("/kody")
 @Transactional
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
 public class welcontroller {
     private final Service service;
 
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @PostMapping("/create")
     public String create(@RequestBody Map<String, String> q){
         service.setQ(q.get("question"));
         return "Create-question";
     }
 
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @PostMapping("/createA")
     public String createA(@RequestBody Map<String, String> a){
         service.setA(a.get("adminquestion"), service.getCounter());
         return "Create-answer";
     }
 
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @GetMapping("/question")
     public String question(){
         return service.getQ(service.getCounter());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @GetMapping("/getQ")
     public String getq(){
-        double id = 5;
-        String datas = service.getQ(id);
-        return datas;
+        return service.getQ(service.getCounter());
     }
 
+    @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
     @PostMapping("/check")
     public boolean check(@RequestBody Map<String, String> q){
 //        System.out.println(q.get("password"));
